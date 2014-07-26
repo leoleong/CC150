3814650523f814edb974d3c48175ba036911d094
leoleong <renzhejiajia@qq.com>
2014-07-26 10:31:16 +0800

    first commit

---
+++ src/cc150/_18_2_PerfectShuffle.java
@@ -0,0 +1,43 @@
+/**
+ * Problem:
+ * Write a method to shuffle a deck of cards. 
+ * It must be a perfect shuffle -- in other words, each of the 52! permutations of the deck has to be equally likely. 
+ * Assume that you are given a random number generator which is perfect.
+ */
+package cc150;
+
+public class _18_2_PerfectShuffle {
+
+	public static int[] shuffleArrayRecursively(int[] cards, int i) {
+
+		if (i == 0) {
+			return cards;
+		}
+
+		shuffleArrayRecursively(cards, i - 1);
+		int k = rand(0, i);
+
+		int temp = cards[k];
+		cards[k] = cards[i];
+		cards[i] = temp;
+
+		return cards;
+	}
+
+	public static int[] shuffleArrayIteratively(int[] cards) {
+
+		for (int i = 0; i < cards.length; i++) {
+			int k = rand(0, i);
+			int temp = cards[k];
+			cards[k] = cards[i];
+			cards[i] = temp;
+		}
+
+		return cards;
+	}
+
+	private static int rand(int lower, int higher) {
+
+		return lower + (int) (Math.random() * (higher - lower + 1));
+	}
+}
